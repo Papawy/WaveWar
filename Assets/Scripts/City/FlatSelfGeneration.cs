dbefd7a2be1e4799479b915cceacc22c53a59070
Papawy <contactpapawy@gmail.com>
2017-02-20 14:51:15 +0100

    Removed Debug log in flatGeneration, added test animations (assets and scripts)

--- Assets/Scripts/City/FlatSelfGeneration.cs
+++ Assets/Scripts/City/FlatSelfGeneration.cs
@@ -32,14 +32,12 @@ public class FlatSelfGeneration : MonoBehaviour {
 		System.Random rnd = new System.Random();
 
 		m_building.Add((GameObject)Instantiate(Grounds[rnd.Next(0, Grounds.Count)], gameObject.transform.position, gameObject.transform.rotation));
-		Debug.Log(m_building[0].GetComponentInChildren<Renderer>().bounds.size.y);
 		int idx = 0;
 		for (int i = 1; i <= Height; i++)
 		{
 			idx = rnd.Next(0, Floors.Count);
 			
 			m_building.Add((GameObject)Instantiate(Floors[idx], gameObject.transform.position + (Vector3.up * m_building[i-1].GetComponentInChildren<Renderer>().bounds.size.y * i), gameObject.transform.rotation));
-			Debug.Log(m_building[i].GetComponentInChildren<Renderer>().bounds.size.y);
 		}
 		idx = rnd.Next(0, Rooftops.Count);
 		m_building.Add((GameObject)Instantiate(Rooftops[idx], gameObject.transform.position + Vector3.up * m_building[m_building.Count - 1].GetComponentInChildren<Renderer>().bounds.size.y * (Height + 1), gameObject.transform.rotation));
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,34 +7,42 @@ public class PlayerController : MonoBehaviour {
 
 	public float Speed = 1.0f;
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float move = TeamUtility.IO.InputManager.GetAxisRaw("Vertical");
+		anim.SetFloat("Speed", move);
 		Quaternion camRot = Quaternion.Euler(0.0f, Camera.main.transform.rotation.eulerAngles.y, 0);
+		if(move != 0.0)
+		{
+			this.transform.rotation = camRot;
+		}
 		if (TeamUtility.IO.InputManager.GetAxisRaw("Vertical") > 0)
 		{
 			this.transform.rotation = camRot;
-			this.transform.position += transform.forward * Speed * Time.deltaTime;
+			//this.transform.position += transform.forward * Speed * Time.deltaTime;
 		}
 		else if (TeamUtility.IO.InputManager.GetAxisRaw("Vertical") < 0)
 		{
 			this.transform.rotation = camRot;
-			this.transform.position -= transform.forward * Speed * Time.deltaTime;
+			//this.transform.position -= transform.forward * Speed * Time.deltaTime;
 		}
 
 		if (TeamUtility.IO.InputManager.GetAxisRaw("Horizontal") < 0)
 		{
 			this.transform.rotation = camRot;
-			this.transform.position -= transform.right * Speed * Time.deltaTime;
+			//this.transform.position -= transform.right * Speed * Time.deltaTime;
 		}
 		else if (TeamUtility.IO.InputManager.GetAxisRaw("Horizontal") > 0)
 		{
 			this.transform.rotation = camRot;
-			this.transform.position += transform.right * Speed * Time.deltaTime;
+			//this.transform.position += transform.right * Speed * Time.deltaTime;
 		}
 
 	}
